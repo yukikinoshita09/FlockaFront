6efb8c6ea2eeda1df01e5b4ec8713ece7f3f78d8
aoilv99 <aoiiiiilv999@gmail.com>
2025-08-27 22:56:55 +0900

    [add] アプリのスタート画面作成

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,38 @@
 import { router } from 'expo-router';
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, Image} from "react-native";
 import "../global.css";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center ">
-      <Pressable onPress={()=>router.navigate('/sign-up')}>
-        <Text className="text-xl font-bold">
-          新規登録
-        </Text>
-      </Pressable>
-      <Pressable onPress={()=>router.navigate('/sign-in')} className="mt-4">
-        <Text className="text-xl font-bold">
-          ログイン
-        </Text>
-      </Pressable>
-    </View>
+      <Image source={require("../assets/images/start-app-icon.png")} 
+      className='w-40 h-40 mb-12'
+      resizeMode='contain'/>
+      <Text className='text-xl font-extrabold mb-48'>flocka</Text>
+      <View className='w-full px-16'>
+        <Pressable onPress={()=>router.navigate('/sign-up')} className='bg-black h-16 items-center justify-center rounded-lg'>
+          <Text className="text-xl font-bold text-white">
+            新規登録
+          </Text>
+        </Pressable>
+      </View>
+      <View className='w-full px-16 '>
+        <Pressable onPress={()=>router.navigate('/sign-in')} className="h-16 items-center justify-center border border-gray-400 mt-12 rounded-lg">
+          <Text className="text-xl font-bold">
+            ログイン
+          </Text>
+        </Pressable>
+      </View>
+      <View className='flex-row mt-8'>
+        <Pressable onPress={() => console.log("click")}>
+          <Text className='text-xs underline'>利用規約</Text>
+        </Pressable>
+        <Text className="text-xs"> と </Text>
+        <Pressable onPress={() => console.log("click")}>
+          <Text className='text-xs underline'>プライバシーポリシー</Text>
+        </Pressable>
+        <Text className="text-xs">に同意してflockaを利用します。</Text>
+      </View>
+      </View>
   );
 }

74e7be430b2da2a479fa76c5d706120136829d7b
kinoshita <171928933+yukikinoshita09@users.noreply.github.com>
2025-08-28 12:08:01 +0900

    [change] インデントの調整とパスワード表示の切り替え追加

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,41 +1,55 @@
-import { Pressable, Text, View, Image, TextInput} from "react-native";
+import { Pressable, Text, View, Image, TextInput, TouchableOpacity} from "react-native";
 import { router } from 'expo-router';
-
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
   return (
     <>
-    <View className="items-center">
-      <Image
-        source={require("../assets/images/start-app-icon.png")}
-        className="w-32 h-32 mt-14"
-        resizeMode="contain">
-      </Image>
-       <Text className='text-4xl font-extrabold mt-10'>flocka</Text>
-    </View>
-      <Text className='text-lg font-extrabold mx-14 mt-16'>メールアドレス</Text>
-    <View className="mx-14 mt-4">
-        <TextInput
-          placeholder="sample@example.com"
-          className="h-14 border border-gray-400 rounded-lg px-4 text-base"
-          keyboardType="email-address"
-        />
+      <View className="items-center">
+        <Image
+          source={require("../assets/images/start-app-icon.png")}
+          className="w-32 h-32 mt-14"
+          resizeMode="contain">
+        </Image>
+        <Text className='text-4xl font-extrabold mt-10'>flocka</Text>
       </View>
-
-      <View className="flex-row">
-        <Text className='text-lg font-extrabold mx-14 mt-8'>パスワード</Text>
-        <Text className="text-sm mt-10 pl-5 text-gray-500">半角英数字のみ・8文字以上</Text>
+      <View className="mx-14 mt-4">
+        <View>
+          <Text className='text-lg font-extrabold mt-16'>メールアドレス</Text>
+          <TextInput
+            placeholder="sample@example.com"
+            className="mt-3 h-14 border border-gray-400 rounded-lg px-4 text-base"
+            keyboardType="email-address"
+          />
+        </View>
+        <View className="mt-6">
+          <View className="flex-row gap-5 items-center">
+            <Text className='text-lg font-extrabold'>パスワード</Text>
+            <Text className="text-sm text-gray-500">半角英数字のみ・8文字以上</Text>
+          </View>
+          <View className="mt-3 h-14 border border-gray-400 rounded-lg px-4 flex-row items-center">
+            <TextInput
+              placeholder="パスワードを入力"
+              className="flex-1 text-base"
+              secureTextEntry={!showPassword}
+            />
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+              <Ionicons
+                name={showPassword ? "eye" : "eye-off"}
+                size={24}
+                color="gray"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Pressable onPress={() => console.log("click")}>
+          <Text className="text-sm mt-3 text-right text-blue-400">パスワードを忘れた方</Text>
+        </Pressable>
       </View>
-      <View className="mx-14 mt-3">
-        <TextInput
-          placeholder="パスワードを入力"
-          className="h-14 border border-gray-400 rounded-lg px-4 text-base"
-          secureTextEntry={true} 
-        />  
-      </View>  
-      <Pressable onPress={() => console.log("click")}>
-        <Text className="text-sm mt-3 left-64 text-blue-400">パスワードを忘れた方</Text>
-      </Pressable>
       <View className='w-full px-16 mt-16'>
         <Pressable onPress={()=> console.log("click")} className='bg-black h-16 items-center justify-center rounded-lg'>
           <Text className="text-xl font-bold text-white">
@@ -48,7 +62,7 @@ export default function SignIn() {
           <Text className="text-xl font-bold">
             新規登録はこちら
           </Text>
-          </Pressable>
+        </Pressable>
       </View>
     </>
   );

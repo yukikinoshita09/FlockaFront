5fc89fece2b720665347855f9696a33d3dd37f16
kazu3jp <kazu3jp.purin@outlook.jp>
2025-08-28 19:24:35 +0900

    [fix] (tabs) スクリーンのオプションから headerShown を削除

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout() {
           <Stack.Screen name="sign-up-auth" options={{ headerShown: false }} />
           <Stack.Screen name="create-card" options={{ headerShown: false }} />
           <Stack.Screen name="card-upload" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
